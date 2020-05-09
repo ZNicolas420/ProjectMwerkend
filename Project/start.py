10ee7073f93bd5fa7953f8d728fd1945a12007ef
ZNicolas420 <zombienicolas@gmail.com>
2020-05-09 21:03:51 +0200

    game disgin lol

--- Project/start.py
+++ Project/start.py
@@ -1,5 +1,15 @@
-print("help")
+p = 100;
 manganaam = input("naam van de manga?")
 thema = input("thema?")
 genre = input("genre?")
 print("naam:" + manganaam + " thema:" + thema + " genre:" + genre)
+print("verhaal?")
+print("dialoog?")
+dialoog = int(input())
+print("karakaters?")
+karakaters = int(input())
+if ((dialoog + karakaters) > p):
+    print ("hoer" )
+else:
+    print("verhaal")
+    print(p-(dialoog + karakaters),"%")
