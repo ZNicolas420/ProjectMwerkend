a481c06d93136858775ca87dfefc5bf38b26005c
ZNicolas420 <zombienicolas@gmail.com>
2020-05-10 20:57:06 +0200

    object af alle af niet echt eh ma bon

--- Project/oneshotmanga.py
+++ Project/oneshotmanga.py
@@ -14,11 +14,19 @@ class Oneshotmanga:
         print("thema : " + self.thema)
         print("genre : " + self.genre)
 
-        print("verhaal    :" , self.dialoog ,"%", self.karakaters ,"%", self.verhaal,"%")
-        print("tekeningen :" , self.achtergronden ,"%", self.persoonage ,"%", self.effecten,"%")
+        print("verhaal    :" , berekenprocent(self.dialoog,self.karakaters,self.verhaal) ,"%", berekenprocent(self.karakaters,self.dialoog,self.verhaal) ,"%", berekenprocent(self.verhaal,self.dialoog,self.karakaters),"%")
+        print("tekeningen :" , berekenprocent(self.achtergronden,self.persoonage,self.effecten) ,"%", berekenprocent(self.persoonage,self.achtergronden,self.effecten) ,"%", berekenprocent(self.effecten,self.achtergronden,self.persoonage),"%")
+
+def berekenprocent(a,b,c):
+    totaal = a+b+c
+    round((a/totaal)*100)
+    return(round((a/totaal)*100))
 
 #manga maken/save info
 #o1 = Oneshotmanga("tuto", "avontuur", "zombie",50,50,50,50,50,50)
 
 #manga gegevens opvragen
 #o1.introduce_self()
+
+
+#berekenprocent(self.effecten,self.achtergronden,self.persoonage)
