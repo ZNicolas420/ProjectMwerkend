cd44b962a60041ff8eb214c5867ccc34f9b1a8a7
ZNicolas420 <zombienicolas@gmail.com>
2020-05-10 16:35:26 +0200

    help gelukkige moederdag

--- Project/start.py
+++ Project/start.py
@@ -1,15 +1,20 @@
+def berekenprocent(a,b,c):
+    totaal = a+b+c
+    andernaam = round((a/totaal)*100)
+    print(andernaam,"%")
+
 p = 100;
 manganaam = input("naam van de manga?")
 thema = input("thema?")
 genre = input("genre?")
 print("naam:" + manganaam + " thema:" + thema + " genre:" + genre)
-print("verhaal?")
 print("dialoog?")
 dialoog = int(input())
 print("karakaters?")
 karakaters = int(input())
-if ((dialoog + karakaters) > p):
-    print ("hoer" )
-else:
-    print("verhaal")
-    print(p-(dialoog + karakaters),"%")
+print("verhaal?")
+verhaal = int(input())
+print("----------------------------------------------------------------------------------------------------------------------")
+print("dialoog :" , format(berekenprocent(dialoog,karakaters,verhaal)))
+berekenprocent(karakaters,dialoog,verhaal)
+berekenprocent(verhaal,karakaters,dialoog)
